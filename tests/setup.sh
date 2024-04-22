3dc128499cff282ae8486d7240922be88e28e84f
Simon Haines <nimon@hotmail.co.uk>
2024-04-22 16:25:53 +0100

    installing sqlite3, but only when needed

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Check if sqlite3 is installed
+if ! command -v sqlite3 &> /dev/null; then
+    echo "sqlite3 could not be found, installing now..."
+        sudo apt update
+        sudo apt install -y sqlite3
+fi
+
 sqlite3 db/db.sqlite < db/reset.sql
